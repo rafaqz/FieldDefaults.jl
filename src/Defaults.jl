9374d0b0559abf36f7f0322e70af13ec8b506c90
Rafael Schouten <rafaelschouten@gmail.com>
2018-07-04 19:14:29 +1000

    working keyword defaults

--- src/Defaults.jl
+++ src/Defaults.jl
@@ -1,5 +1,40 @@
+__precompile__()
+
 module Defaults
 
-# package code goes here
+using MetaFields
+
+export @default, @kw_default, default, kw_default
+
+@metafield default nothing
+
+macro kw_default(ex)
+    typ = MetaFields.firsthead(ex, :type) do typ_ex
+        return MetaFields.namify(typ_ex.args[2])
+    end |> esc
+
+    quote 
+        import Defaults.default
+        @default $(esc(ex))
+        $typ(;kwargs...) = kw_default($typ; kwargs...)
+    end
+end
+
+kw_default(::Type{T}; kwargs...) where T = begin
+    dict = Dict()
+    fnames = fieldnames(T)
+    for fname in fnames
+       dict[fname] = get_default(T, Val{fname})
+    end
+
+    for (key, val) in kwargs
+        key in fnames || error("$key is not a field of $T")
+        dict[key] = val
+    end
+
+    T(getindex.(dict, fnames)...)
+end
+
+get_default(T::Type, F::Type) = default(T, F) 
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,24 @@
 using Defaults
+using Defaults: get_default
+
 @static if VERSION < v"0.7.0-DEV.2005"
     using Base.Test
 else
     using Test
 end
 
-# write your own tests here
-@test 1 == 2
+@kw_default struct Test
+    foo::Int    | 1
+    bar::Symbol | :bar
+end
+
+@test get_default(Test, Val{:foo}) == 1
+@test get_default(Test, Val{:bar}) == :bar
+
+t = Test()
+@test t.foo == 1
+@test t.bar == :bar
+
+t = Test(foo = 2, bar=:foobar)
+@test t.foo == 2
+@test t.bar == :foobar
