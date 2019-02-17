4185f66ec27caa83edbe1b681776a51406c362eb
Rafael Schouten <rafaelschouten@gmail.com>
2019-02-18 03:20:52 +1100

    handle mixed types, add macro for combinedf default and units]

--- src/FieldDefaults.jl
+++ src/FieldDefaults.jl
@@ -1,45 +1,57 @@
 module FieldDefaults
 
-using FieldMetadata
-using FieldMetadata: @default, @redefault, default
+using FieldMetadata, Setfield
+using FieldMetadata: @default, @redefault, default, units
+using Base: tail
 
-export @default_kw, default_kw
+export @default_kw, @udefault_kw, @redefault_kw, @reudefault_kw, default_kw, udefault_kw
 
 macro default_kw(ex)
-    typ = get_type(ex)
-    quote
-        import FieldDefaults.default
-        $(FieldMetadata.add_field_funcs(ex, :default))
-        $(esc(typ))(;kwargs...) = default_kw($(esc(typ)); kwargs...)
-    end
+    default_kw_macro(ex, :default_kw, false)
 end
 
 macro redefault_kw(ex)
-    typ = get_type(ex)
+    default_kw_macro(ex, :default_kw, true)
+end
+
+macro udefault_kw(ex) default_kw_macro(ex, :udefault_kw, false) end
+
+macro reudefault_kw(ex)
+    default_kw_macro(ex, :udefault_kw, true)
+end
+
+default_kw_macro(ex, func, update) = begin
+    typ = FieldMetadata.firsthead(ex, :struct) do typ_ex
+        FieldMetadata.namify(typ_ex.args[2])
+    end
     quote
         import FieldDefaults.default
-        $(FieldMetadata.add_field_funcs(ex, :default; update=true))
-        $(esc(typ))(;kwargs...) = default_kw($(esc(typ)); kwargs...)
+        $(FieldMetadata.add_field_funcs(ex, :default; update=update))
+        $(esc(typ))(;kwargs...) = $func($(esc(typ)); kwargs...)
     end
 end
 
-get_type(ex) =
-    FieldMetadata.firsthead(ex, :struct) do typ_ex
-        return FieldMetadata.namify(typ_ex.args[2])
-    end
-
-default_kw(::Type{T}; kwargs...) where T = begin
+insert_kwargs(kwargs, defaults, T) = insert_kwargs(keys(kwargs.data), Tuple(kwargs.data), defaults, T)
+insert_kwargs(keys::Tuple, vals, defaults, T) = begin
     fnames = fieldnames(T)
-    defaults = get_default(T)
-
-    for (key, val) in kwargs
-        key in fnames || error("$key is not a field of $T")
-        ind = findfirst(n -> n == key, fnames)
-        defaults = (defaults[1:ind-1]..., val, defaults[ind+1:end]...)
-    end
-
-    T(defaults...)
+    key, val = keys[1], vals[1]
+    key in fnames || error("$key is not a field of $T")
+    ind = findfirst(n -> n == key, fnames)
+    @set! defaults[ind] = val
+    insert_kwargs(tail(keys), tail(vals), defaults, T)
 end
+insert_kwargs(keys::Tuple{}, vals, defaults, T) = defaults
+
+default_kw(::Type{T}; kwargs...) where T =
+    T(insert_kwargs(kwargs, get_default(T), T)...)
+
+# Combined default() and units()
+udefault_kw(::Type{T}; kwargs...) where T =
+    T(insert_kwargs(kwargs, apply_units(get_default(T), units(T)), T)...)
+
+apply_units(defs::Tuple, units::Tuple) = (defs[1] * units[1], apply_units(tail(defs), tail(units))...)
+apply_units(defs::Tuple, units::Tuple{Number,Vararg}) = (defs[1], apply_units(tail(defs), tail(units))...)
+apply_units(defs::Tuple{}, units::Tuple{}) = ()
 
 get_default(args...) = default(args...)
 
